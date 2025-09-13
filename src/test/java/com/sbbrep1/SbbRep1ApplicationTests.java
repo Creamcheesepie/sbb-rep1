cc8e8d2f32a9cf8c87611626160e7191e1e2276c
Creamcheesepie <gnldbs1004@naver.com>
2025-09-13 17:38:58 +0900

    ignore file에 DB 파일 추가, 중간 작업 갱신

--- src/test/java/com/sbbrep1/SbbRep1ApplicationTests.java
+++ src/test/java/com/sbbrep1/SbbRep1ApplicationTests.java
@@ -30,17 +30,20 @@ class SbbRep1ApplicationTests {
         if(cnt > 0) return ;
         Question q1 = new Question("질문을 합니다","프로그래밍은 즐거운가요?");
         Question q2 = new Question("OCP 원칙에 대해","OCP 원칙이 어렵습니다. 예시를 잘 알려주세요");
+        Question q3 = new Question("수정용 데이터 입니다.","내용을 수정해 주세요.");
+
         questionRepository.save(q1);
         questionRepository.save(q2);
+        questionRepository.save(q3);
         List<Question> all = this.questionRepository.findAll();
-        assertEquals(2,all.size());
+        assertEquals(3,all.size());
     }
 
     @Test
     @DisplayName("질문 데이터 조회하기")
     void t2(){
         List<Question> all = this.questionRepository.findAll();
-        assertEquals(2,all.size());
+        assertEquals(3,all.size());
 
         Question q1 = all.get(0);
         assertEquals("질문을 합니다", q1.getSubject());
@@ -86,5 +89,15 @@ class SbbRep1ApplicationTests {
         }
     }
 
+    @Test
+    @DisplayName("질문 데이터 수정하기")
+    void t7(){
+        Long id = 2L;
+        Optional<Question> oq = this.questionRepository.findById(0L);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        q.setSubject("수정한 제목입니다.");
+        this.questionRepository.save(q);
+    }
 
 }
