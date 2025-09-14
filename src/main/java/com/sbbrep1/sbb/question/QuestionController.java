95a1dccec9d143720be9098a692fe23858bac926
Creamcheesepie <gnldbs1004@naver.com>
2025-09-14 16:55:23 +0900

    상세페이지 완성

--- src/main/java/com/sbbrep1/sbb/question/QuestionController.java
+++ src/main/java/com/sbbrep1/sbb/question/QuestionController.java
@@ -1,22 +1,34 @@
 package com.sbbrep1.sbb.question;
 
+import com.sbbrep1.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class QuestionController {
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
 
     @GetMapping("/question/list")
     public String list(Model model){
-        List<Question> questionList = questionRepository.findAll();
+        List<Question> questionList = questionService.getList();
         model.addAttribute("questionList",questionList);
         return "question_list";
+    }
+
+    @GetMapping("/qeuestion/detail/{id}")
+    public String detail(
+            Model model,
+            @PathVariable Long id
+    ) throws DataNotFoundException {
+        Question question = this.questionService.getQuestionByid(id);
+        model.addAttribute("question",question);
+        return "question_detail";
     }
 
 }
---
+++ src/main/java/com/sbbrep1/sbb/question/QuestionService.java
@@ -0,0 +1,29 @@
+package com.sbbrep1.sbb.question;
+
+import com.sbbrep1.sbb.DataNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList(){
+        return questionRepository.findAll();
+    }
+
+    public Question getQuestionByid(Long id) throws DataNotFoundException {
+        Optional<Question> question = questionRepository.findById(id);
+        if(question.isPresent()){
+            return question.get();
+        } else{
+            throw new DataNotFoundException("question not found");
+        }
+
+
+    }
+}
